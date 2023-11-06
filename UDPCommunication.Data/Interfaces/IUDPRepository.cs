40aa4378f03e765aabfee3e2753730cc1f1bccb4
cusfekes <cusfekes@havelsan.com.tr>
2023-11-06 18:01:44 +0300

    DI codes

---
+++ UDPCommunication.Data/Interfaces/IUDPRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UDPCommunication.Models;
+using UDPCommunication.Models.DomainModels;
+
+namespace UDPCommunication.Data.Interfaces
+{
+    public interface IUDPRepository
+    {
+        OperationResult<List<Derece>> GetAllItems();
+
+        List<Derece> GetItemsByDateRange(DateTime startDate, DateTime endDate);
+
+        bool SaveItem(Derece item);
+
+        bool DeleteItem(Guid itemId);
+    }
+}
---
+++ UDPCommunication.Data/Repository/UDPRepository.cs
@@ -0,0 +1,55 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UDPCommunication.Data.Interfaces;
+using UDPCommunication.Models;
+using UDPCommunication.Models.DomainModels;
+
+namespace UDPCommunication.Data.Repository
+{
+    public class UDPRepository : IUDPRepository
+    {
+        public bool DeleteItem(Guid itemId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public OperationResult<List<Derece>> GetAllItems()
+        {
+            OperationResult<List<Derece>> result = new OperationResult<List<Derece>>();
+            try
+            {
+                var configuration = new Configuration();
+                ISessionFactory sessionFactory = new Configuration().Configure("hibernate.cfg.xml").BuildSessionFactory();
+                using (ISession session = sessionFactory.OpenSession())
+                {
+                    using (ITransaction transaction = session.BeginTransaction())
+                    {
+                        List<Derece> source = session.CreateSQLQuery(@"select * from ""SIC"".""Derece""").AddEntity(typeof(Derece)).List<Derece>().ToList();
+                        result.SetSuccessMode(source);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                result.SetFailureMode(ex);
+            }
+            return result;
+        }
+
+        public List<Derece> GetItemsByDateRange(DateTime startDate, DateTime endDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveItem(Derece item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
